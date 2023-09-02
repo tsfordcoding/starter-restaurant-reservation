1ed3c889a93363fe428549330f655a0d90d37c4d
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-09-02 16:43:01 -0700

    add create function and start validation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,6 +4,34 @@
 const reservationsService = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+function hasData(req, res, next) {
+  if(req.body.data) {
+    return next();
+  }
+  next({ 
+    status: 400, 
+    message: "body must have data property" 
+  });
+}
+
+function hasFirstName(req, res, next) {
+  if(req.body.data.first_name) {
+    return next();
+  } 
+  next({
+    status: 400,
+    message: "first_name",
+  })
+}
+
+async function create(req, res, next) {
+  const newReservation = await reservationsService.create(req.body.data);
+
+  res.status(201).json({
+    data: newReservation,
+  });
+}
+
 async function list(req, res) {
   const { date } = req.query;
     
@@ -13,4 +41,5 @@ async function list(req, res) {
 
 module.exports = {
   list: [asyncErrorBoundary(list)],
+  create: [hasData, hasFirstName, asyncErrorBoundary(create)],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,14 @@
 const knex = require("../db/connection");
 
+const create = (newReservation) => {
+    return knex("reservations").insert(newReservation).returning("*");
+}
+
 const list = (date) => {
     return knex("reservations").where("reservation_date", date).select("*").orderBy("reservation_time");
 }
 
 module.exports = {
     list,
+    create,
 }
