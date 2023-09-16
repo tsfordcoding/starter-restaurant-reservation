b7da2bc3bf888739bdbf081e9cd3a2633fc35f14
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-09-16 13:15:39 -0700

    completed migrations and seed for tables

---
+++ back-end/src/db/seeds/01-tables.js
@@ -0,0 +1,7 @@
+const tablesData = require("./01-tables.json");
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(() => knex("tables").insert(tablesData));
+};
